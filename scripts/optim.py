c378b80960ad5991e500c22a8589ed72b40d0dc9
AhrixRain <ziqingli1027@gmail.com>
2025-11-17 13:45:04 -0800

    cvrewgwe

--- scripts/optim.py
+++ scripts/optim.py
@@ -321,8 +321,8 @@ def _prepare_wandb_logger(args: argparse.Namespace, trial_index: int, options: O
 
     logger = WandbLogger(
         project=args.wandb_project,
-        name=f"hpst_sherpa_trial_{trial_index}_R5",
-        id=f"hpst_optim_server_{trial_index}_R5",
+        name=f"hpst_sherpa_trial_{trial_index}_R6",
+        id=f"hpst_optim_server_{trial_index}_R6",
         save_dir=str(base_dir.parent),
     )
     update_config(logger, vars(options))
